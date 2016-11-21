b410244834a80517b30e8913bc36ad7ba8bb5bdb
Shilpa Joshi <shilpa.joshi@episerver.com>
2016-11-21 12:17:07 -0500

    added a productviewmodel for productpage, radiobuttonlist to view and a productpagecontroller

---
+++ src/EPiServer.SocialAlloy.Web/Controllers/ProductPageController.cs
@@ -0,0 +1,28 @@
+﻿using EPiServer.Framework.DataAnnotations;
+using EPiServer.SocialAlloy.Web.Models.Pages;
+using EPiServer.SocialAlloy.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPiServer.SocialAlloy.Web.Controllers
+{
+    [TemplateDescriptor(Inherited = true)]
+    public class ProductPageController : PageControllerBase<ProductPage>
+    {
+        public ViewResult Index(ProductPage currentPage)
+        {
+            var model = ProductViewModel.Create(currentPage);
+            @ViewBag.PageReference = currentPage.ContentLink;
+            return View(string.Format("~/Views/{0}/Index.cshtml", currentPage.GetOriginalType().Name), model);
+        }
+
+        [HttpPost]
+        public ActionResult Rate(ProductPage currentContent, int? userRating)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/EPiServer.SocialAlloy.Web/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using EPiServer.SocialAlloy.Web.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPiServer.SocialAlloy.Web.Models.ViewModels
+{
+    public class ProductViewModel<T> : PageViewModel<T> where T : SitePageData
+    {
+        public ProductViewModel(T currentPage)
+            : base(currentPage)
+        {
+            PageRatings = new List<int>() { 1, 2, 3, 4, 5 };
+        }
+
+        public String CurrentUserName { get; set; }
+        public String PageId { get; set; }
+
+        public List<int> PageRatings { get; set; }
+
+        public int? UserRating { get; set; }
+
+    }
+    public static class ProductViewModel
+    {
+        public static ProductViewModel<T> Create<T>(T page) where T : SitePageData
+        {
+            return new ProductViewModel<T>(page);
+        }
+
+    }
+}
