27b130984b12c1b2729847f487782edbc385ca7d
Carlos Colon <carlos.colon@episerver.com>
2017-01-27 13:18:46 -0500

    Implement a Like Button tutorial (#33)
    Implemented a Like Button tutorial using the EPiServer Social Rating service API.
    
    * Address code review comments.

---
+++ src/EPiServer.SocialAlloy.Web/Social/Tutorials/LikeButton/LikeButtonBlockViewModel.cs
@@ -0,0 +1,33 @@
+﻿using EPiServer.Core;
+
+namespace EPiServer.SocialAlloy.Web.Social.Models
+{
+    /// <summary>
+    /// The LikeButtonBlockViewModel class represents the model that will be used to
+    /// provide data to the Like button block frontend view.
+    /// </summary>
+    public class LikeButtonBlockViewModel
+    {
+        /// <summary>
+        /// Parameterless constructor -- required by the EPiServer CMS framework.
+        /// </summary>
+        public LikeButtonBlockViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the current page link.
+        /// </summary>
+        public PageReference PageLink { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number of Liked ratings found for the current page.
+        /// </summary>
+        public long TotalCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the existing Liked rating, if any, submitted by current user for the current page.
+        /// </summary>
+        public int? CurrentRating { get; set; }
+    }
+}
